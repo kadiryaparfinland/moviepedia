a8dbbbf2c75ade137cf3715246bf0443c0d8b39d
andromegasoftware <kadiryapar16@gmail.com>
2023-03-12 15:42:14 +0200

    sci-fic movies is corrected

--- src/app/services/my-data.service.ts
+++ src/app/services/my-data.service.ts
@@ -22,7 +22,7 @@ export class MyDataService {
   }
 
   getScienceFictionMovies() {
-    return this.http.get('https://api.themoviedb.org/3/discover/tv?api_key=dc92a7524c83f242a3237b2222941f00&language=en-US&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&with_genres=10765&include_null_first_air_dates=false');
+    return this.http.get('https://api.themoviedb.org/3/discover/movie?api_key=dc92a7524c83f242a3237b2222941f00&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=878');
   }
 
   getFamilyMovies() {
